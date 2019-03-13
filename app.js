053a7293cd94e2abc5cd4cee87fd09d5a49414d8
Fahad <kpfahadmon@gmail.com>
2019-03-13 17:06:21 +0530

    Signed-off-by: Fahad <kpfahadmon@gmail.com>

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ let executeNewman = (collectionFile,environmentFile, iterationCount) => {
             collection: collectionFile,
             environment: environmentFile,
             iterationCount: iterationCount,
-            reporters: ['htmlextra'],
+            reporters: ['htmlextra-BuyerService'],
             reporter: {
                 htmlextra: {
                     export: './reports/htmlResults.html'
